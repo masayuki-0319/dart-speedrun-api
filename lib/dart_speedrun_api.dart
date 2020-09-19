ae4c0d8354bf549cebbc1046866d36078356d768
Masayuki Onishi <omccello@gmail.com>
2020-09-19 13:36:04 +0900

    initial commit

---
+++ lib/dart_speedrun_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_speedrun_api;
+
+export 'src/dart_speedrun_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
